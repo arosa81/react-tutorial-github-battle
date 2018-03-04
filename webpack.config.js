d75a13e80142608c1cb582003f6b9a50d403296d
Alexander Rosa <alex.j.rosa@gmail.com>
2018-03-04 12:52:49 -0700

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: __dirname + '/app/index.html',
-    filename: 'index.html',
-    inject: 'body'
-})
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [
-        './app/index.js'
+  entry: [
+    './app/index.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
-    output: {
-        path: __dirname + '/dist',
-        filename: "index_bundle.js"
-    },
-    module: {
-        loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-        ]
-    },
-    plugins: [HtmlWebpackPluginConfig]
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+    }),
+  ],
+};
