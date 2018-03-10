fb742da3dddf2f8bc34d2f47e7470e3f81638974
Alexander Rosa <alex.j.rosa@gmail.com>
2018-03-10 13:22:47 -0700

    created Home component and styling

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Popular from './Popular';
+import Home from './Home';
 import Nav from './Nav';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <Router>
         <div className="container">
           <Nav />
+          <Route exact path="/" component={Home} />
           <Route path="/popular" component={Popular} />
         </div>
       </Router>
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,20 +1,15 @@
-const React = require('react');
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const transparentBg = require('../styles').transparentBg;
-const ReactRouter = require('react-router');
-const Link = ReactRouter.Link;
 
-var Home = React.createClass ({
-    render: function () {
-        return (
-            <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
-                <h1>Github Battle</h1>
-                <p className="lead">Some fancy motto</p>
-                <Link to="/playerOne">
-                    <button type="button" className="btn btn-lg btn-success">Get Started</button>
-                </Link>
-            </div>
-        )
-    }
-})
-
-module.exports = Home;
+export default function Home(props) {
+  return (
+    <div className="home-container" style={transparentBg}>
+      <h1>Github Battle: Battle your friends...and stuff.</h1>
+      <Link className="button" to="/battle">
+        Battle
+      </Link>
+    </div>
+  );
+}
